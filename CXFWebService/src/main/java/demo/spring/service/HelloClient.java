635fcb1b7bb22cc58ad8c41636475378c1dc3e31
Vineeta Khardia <vinkhardia@gmail.com>
2017-06-08 20:42:44 -0400

    changed the config file argument name

--- CXFWebService/src/main/java/demo/spring/service/HelloClient.java
+++ CXFWebService/src/main/java/demo/spring/service/HelloClient.java
@@ -7,7 +7,7 @@ public class HelloClient {
 	
 	public static void main(String[] args) {
 		
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"webapp/WEB-INF/spring-config.xml"});
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"spring-config.xml"});
 		HelloWorld client = (HelloWorld)context.getBean("helloClient");
 		client.sayHi("World");
 		
